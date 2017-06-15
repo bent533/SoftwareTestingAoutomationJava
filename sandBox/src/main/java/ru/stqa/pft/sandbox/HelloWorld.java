660e32d9218e407a7b507df9163f1df1dcbe3009
bent533 <bent533@gmail.com>
2017-06-15 07:43:08 +0300

    запустил в отладчике прогу

--- sandBox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandBox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
+		int a=0;
+		int b=5*5;
+		int c=b+a+189;
 		System.out.println("Hello, wAnna!");
 	}
 }
