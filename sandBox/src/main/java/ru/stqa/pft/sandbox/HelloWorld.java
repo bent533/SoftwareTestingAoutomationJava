29e7c4ef4f8cbde42e03cfe5f58e250b84615032
bent533 <bent533@gmail.com>
2017-06-16 07:25:09 +0300

    2 функции накодил стрингу и дабл би ай эр н БИ :)

--- sandBox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandBox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -2,11 +2,32 @@ package ru.stqa.pft.sandbox;
 
 public class HelloWorld {
 
-	public static void main(String[] args){
-		System.out.println("Hello, world!");
+	public static void main(String[] args)
+	{
 		int a=0;
 		int b=5*5;
-		int c=b+a+189;
+
+
+		textPrint("I use my first function");
+		System.out.println("Simple calcFunction c = " + simpleCalc(58,156458));
+
+	}
+
+
+	public static void textPrint(String stringa)
+	{
+		System.out.println(stringa);
 		System.out.println("Hello, wAnna!");
 	}
+
+	public static double simpleCalc(double a, double b)
+	{
+		double c;
+		c	= b+a+189;
+		return  c;
+	}
+
 }
+
+
+
