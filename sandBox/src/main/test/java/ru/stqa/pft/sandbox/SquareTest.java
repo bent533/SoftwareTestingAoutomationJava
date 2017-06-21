bedf979f9182dcdc9bab5c2137da808c9b125e74
bent533 <bent533@gmail.com>
2017-06-21 07:48:50 +0300

    Сделал тест с Assert

---
+++ sandBox/src/main/test/java/ru/stqa/pft/sandbox/SquareTest.java
@@ -0,0 +1,27 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Сергей on 21.06.2017.
+ */
+public class SquareTest
+{
+
+  @Test
+  public void testArea()
+  {
+    Square s = new Square(25);
+    assert s.area() == 62;
+
+  }
+
+  public void testArea2()
+  {
+    Square s = new Square(25);
+    assert s.area() == 625;
+
+  }
+
+
+}
