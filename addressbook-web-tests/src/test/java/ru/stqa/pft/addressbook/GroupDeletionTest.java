05a18fc06a43e2ec2ae33dcb3b8fda72b5f767e1
bent533 <bent533@gmail.com>
2017-06-30 14:15:22 +0300

    Замутил признак наследования в классах

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTest.java
@@ -0,0 +1,26 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTest extends TestBase {
+
+
+
+    
+    @Test
+    public void testGroupDeletion () {
+      /*  wd.get("http://localhost/addressbook/group.php");
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).sendKeys("\\undefined");
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+        wd.findElement(By.name("se lected[]")).click();
+        wd.findElement(By.xpath("//div[@id='content']/form/input[5]")).click();*/
+
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
+
+}
