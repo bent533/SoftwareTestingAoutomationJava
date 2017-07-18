e4dc44196062248951fd774aada4b33ab0410cff
bent533 <bent533@gmail.com>
2017-07-18 19:49:08 +0300

    создал структуру для формы Contact

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,7 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+/**
+ * Created by Сергей on 18.07.2017.
+ */
+public class ContactHelper {
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/ContactCreationTest.java
@@ -0,0 +1,7 @@
+package ru.stqa.pft.addressbook.test;
+
+/**
+ * Created by Сергей on 18.07.2017.
+ */
+public class ContactCreationTest {
+}
